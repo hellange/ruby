66c2da0e32df5608c0a4eedaaaee3f80398ddd3b
hellange <hellange@cisco.com>
2014-02-12 18:13:06 +0100

    Commiting working rails ajax example based on http://richonrails.com/articles/basic-ajax-in-ruby-on-rails

---
+++ rails_ajax/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ajax::Application.config.secret_key_base = '6d3297be00599c5e50971276d6a5c32d5d0f1e71f7ef30d1ffd4040b77dfb5a300e7d43737ba0784b0afa44258b2733f41b20231a36af78ce331a6912e6c3a11'
---
+++ rails_ajax/config/routes.rb
@@ -0,0 +1,7 @@
+Ajax::Application.routes.draw do
+  resources :products do
+    get "delete"
+  end
+ 
+  root to: "products#index"
+end
